96829b073136629d55f2b82c07a10ff3624a1e60
Jeremiah Buddenhagen <bitspill+github@bitspill.net>
2017-01-31 05:32:48 -0600

    Add icons and metadata to media listing

--- src/components/MediaListItem.vue
+++ src/components/MediaListItem.vue
@@ -1,9 +1,23 @@
 <template>
   <div>
     <router-link :to="linkRoute">
-      <div class="float-left on-left"><img src="../assets/video-icon.svg"/></div>
+      <div class="float-left on-left type-icon">
+        <img v-if="type=='video'" src="../assets/video-icon.svg"/>
+        <img v-if="type=='podcast'" src="../assets/podcast-icon.svg"/>
+        <img v-if="type=='thing'" src="../assets/things-icon.svg"/>
+        <img v-if="type=='recipe'" src="../assets/recipe-icon.svg"/>
+        <img v-if="type=='book'" src="../assets/book-icon.svg"/>
+        <img v-if="type=='album'" src="../assets/album-icon.svg"/>
+        <img v-if="type=='music'" src="../assets/song-icon.svg"/>
+        <img v-if="type=='movie'" src="../assets/movie-icon.svg"/>
+      </div>
       <span>{{title}}</span><br>
-      <span>{{year}} • {{publisher}} • {{runtime}} • {{pubTime}}</span>
+      <span>
+        {{year}}
+        • {{publisher}}
+        <span v-if="runtime && (type=='video' || type=='music')">• {{runtime}}</span>
+        • {{pubTime}}
+      </span>
     </router-link>
   </div>
 </template>
@@ -17,7 +31,24 @@
     },
     props: ['m'],
     computed: {
+      type () {
+        // ToDo: if music type check files array to determine if album
+        if (this.oip) {
+          return this.m.type
+        }
+        if (this.am) {
+          return this.am.type
+        }
+        return '<unknown>'
+      },
       year () {
+        let am = this.am
+        if (am !== undefined) {
+          return am.info.year
+        }
+        if (this.oip !== undefined) {
+          return this.m.year
+        }
         return '<unknown>'
       },
       publisher () {
@@ -30,6 +61,25 @@
         return '<unknown>'
       },
       runtime () {
+        if (this.am !== undefined) {
+          let mediaInfo = this.am.info
+          let mediaRuntime
+          if (mediaInfo['extra-info']) {
+            if (mediaInfo['extra-info']['runtime']) {
+              mediaRuntime = amCalcRuntime(mediaInfo['extra-info']['runtime'])
+            }
+            else if (mediaInfo['extra-info']['files']) {
+              mediaRuntime = amCalcRuntime(mediaInfo['extra-info']['files'][0]['duration'])
+            }
+          }
+          if (mediaRuntime !== undefined && (mediaRuntime !== 0) && (mediaRuntime !== '00:00:undefined')) {
+            mediaRuntime = mediaRuntime.split('.')[0]
+          }
+          else {
+            mediaRuntime = ''
+          }
+          return mediaRuntime
+        }
         return '<unknown>'
       },
       pubTime () {
@@ -75,7 +125,36 @@
 
     }
   }
+
+  // CALCULATE RUNTIME FROM SECONDS
+  function amCalcRuntime (seconds) {
+    let runSecs = seconds
+    let runMins = 0
+    let runHours = 0
+    if (runSecs > 59) {
+      runMins = Math.floor(parseInt(seconds) / 60)
+      runSecs = runSecs - (runMins * 60)
+    }
+    if (runSecs < 10) {
+      runSecs = '0' + runSecs
+    }
+    if (runMins > 59) {
+      runHours = Math.floor(parseInt(runMins) / 60)
+      runMins = runMins - (runHours * 60)
+    }
+    if (runMins < 10) {
+      runMins = '0' + runMins
+    }
+    if (runHours < 10) {
+      runHours = '0' + runHours
+    }
+    return runHours + ':' + runMins + ':' + runSecs
+  }
 </script>
 
 <style>
+  .type-icon img {
+    width: 40px;
+    height: 30px;
+  }
 </style>
